87bf684a13e2a170dbfdd5e997f63828a9c46cb6
Maxime Bélair <maxime.belair@orange.com>
2020-03-06 17:25:03 +0100

    Fix: adding missing file ine repo
    Signed-off-by: Maxime Bélair <maxime.belair@orange.com>

---
+++ include/linux/lsmpp.h
@@ -0,0 +1,37 @@
+#ifndef _LSMPP_H
+#define _LSMPP_H
+#include <linux/filter.h>
+#include <linux/bpf.h>
+
+#ifdef CONFIG_SECURITY_LSMPP
+int lsmpp_prog_attach(const union bpf_attr* attr, struct bpf_prog* prog);
+/*
+struct lsmpp_bprm_ctx {
+    struct linux_binprm *bprm;
+};
+struct lsmpp_file_ctx {
+    struct file* file;
+};
+struct lsmpp_mmap_ctx {
+    struct file* file;
+    unsigned long reqprot;
+    unsigned long prot;
+    unsigned long flags;
+};
+*/
+extern const struct bpf_func_proto lsmpp_dynamic_call_proto;
+/*struct lsmpp_ctx {
+    union {
+        struct lsmpp_bprm_ctx bprm_ctx;
+        struct lsmpp_file_ctx file_ctx;
+        struct lsmpp_mmap_ctx mmap_ctx;
+        // TODO more stuff to add!
+    };
+};*/
+#else
+static inline int lsmpp_prog_attach(const union bpf_attr* attr, struct bpf_prog* prog) {
+    return -EINVAL;
+}
+#endif
+
+#endif
